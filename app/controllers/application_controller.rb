a8f354825033d935eba19b8b191f7c92f8577803
Mark Ertmer <mark.ertmer@gmail.com>
2022-04-16 21:24:03 -0600

    Add: current user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_user
+    if !current_user
+      flash[:alert] = 'you must be logged in'
+      redirect_to root_path
+    end
+  end
 end
