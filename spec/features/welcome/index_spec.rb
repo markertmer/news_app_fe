e9b75f385daf5f8019270bd9735903bba992d148
Mark Ertmer <mark.ertmer@gmail.com>
2022-04-16 21:26:58 -0600

    Add: welcome index tests WIP

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -4,10 +4,23 @@ RSpec.describe 'welcome index page' do
 
   it 'exists' do
     visit '/'
-
     expect(current_path).to eq('/')
+  end
 
-    expect(page).to have_content("Get Yo Newz Hurr")
+  it 'has introductory info' do
+    expect(page).to have_content("SPINSPOT")
+    expect(page).to have_content("Exposing Bias in News Reporting")
+    expect(page).to have_content("In an age of information overload")
+  end
+
+  xit 'displays three articles' do
+
+  end
+
+  it 'has a button to sign up or sign in' do
+    expect(page).to have_button("Sign Up or Sign In to Spot the Spin!")
+    click_button("Sign Up or Sign In to Spot the Spin!")
+    # expect(current_path).to eq("/auth/google_oauth2")
   end
 
 end
