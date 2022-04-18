e31603e0180f3aa4cb6527a26197177720a9e1a7
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-18 14:20:46 -0500

    test: user model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+  end
+
 end
