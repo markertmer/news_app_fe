5737b21bc5cd8cc8ebd82311c524f79c317a59ac
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-18 17:07:50 -0400

    remove space

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   def index
     keyword = ["biden", "putin", "supreme court", "will smith", "billionaires"].sample
     topic = TopicFacade.get_articles(keyword)
-    @articles = topic[:articles]
-    @name = topic[:topic]
+    @articles = topic.articles
+    @name = topic.topic
   end
 end
