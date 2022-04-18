d0dad46fd869e994298c2471d3dfa89b5e34acc2
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-18 17:33:27 -0400

    commit before dinner

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -18,13 +18,23 @@ RSpec.describe 'User Dashboard', type: :feature do
     stub_request(:get, "https://news-app-be.herokuapp.com/api/v1/news?keyword=billionares")
     .to_return(status: 200, body: json_response)
 
-    oauth_response = File.read('spec/fixtures/omni_auth_stub.json')
-    stub_request(:post, "/auth/google_oauth2")
-    .to_return(status: 200, body: oauth_response)
+    # oauth_response = File.read('spec/fixtures/omni_auth_stub.json')
+    # stub_request(:post, "/auth/google_oauth2")
+    # .to_return(status: 200, body: oauth_response)
 
-    visit '/'
+    user = User.new(name: 'Paul', email: 'paulleonard4657@gmail.com')
 
-    click_button("Sign Up or Sign In to Spot the Spin!")
+    allow_any_instance_of(Request).to receive().and_return({
+      "session_id": "e970d27ef6cdc5b22d47642e9a71b36a",
+      "_csrf_token": "KVtxm+gC4ep0EV3P2NMPzHh+AVTUu3L7XXJYdlXSJAU=",
+      "csrf": "kg705NcBevp0Qe2nIdcYPimMe8VNxAxeMR9n3HnY2qE=",
+      "access_token": "ya29.A0ARrdaM_X1pW-qB0b2LyVJz-NfFTd4AykPXDlDrTF2HNtjjlCW1xAJ2YrwnsLNR04mE2Rh6IUPyUUOprSuIOxf1tVlbu5j_fkW9OxRsmjGsIf-HQTUSSir2dK5LB6I2Zy8WlTkC1YKXGp-3SspTWGiu_8jvnQ",
+      "user_id": user.id
+      })
+
+    visit '/dashboard'
+
+    # click_button("Sign Up or Sign In to Spot the Spin!")
 
     # allow_any_instance_of(UsersController).to receive(:foo).and_return(:bar)
 
