c5eb2c1d577c09bb1d0b1be85ee3947a51044ebe
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-19 18:07:23 -0400

    All dashboard tests passing and oauth fake authentication implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
     email = auth_hash['info']['email']
-    name = auth_hash['info']['first_name']
+    name = auth_hash['info']['name'] 
     user = User.find_or_create_by(email: email, name: name)
     session[:access_token] = auth_hash['credentials']['token']
     session[:user_id] = user.id
