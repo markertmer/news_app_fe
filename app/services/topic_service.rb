24dab110f3d931c214c46e67cb5a0a35b9c61a63
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-19 18:02:55 -0500

    feat: cache TopicService search results with keyword

--- app/services/topic_service.rb
+++ app/services/topic_service.rb
@@ -1,8 +1,10 @@
 class TopicService
 
   def self.get_data(keyword)
-    url = "https://news-app-be.herokuapp.com/api/v1/news?keyword=#{keyword}"
-    response = Faraday.get(url)
-    data = JSON.parse(response.body, symbolize_names: true)
+    Rails.cache.fetch("get_data_#{keyword}", expires_in: 24.hours) do
+      url = "https://news-app-be.herokuapp.com/api/v1/news?keyword=#{keyword}"
+      response = Faraday.get(url)
+      data = JSON.parse(response.body, symbolize_names: true)
+    end
   end
 end
