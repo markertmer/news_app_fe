93f6af4c81b0667903d84512dd266050cfc10b7c
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-21 17:49:01 -0400

    finish

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,14 +1,16 @@
 class TopicsController < ApplicationController
 before_action :require_user
 
-def search
-  keyword = params[:keyword]
-  if keyword == ""
-    redirect_to('/dashboard')
-    flash.notice = "Field cannot be blank"
-  else
-    topic = TopicFacade.get_articles(keyword)
-    @articles = topic.articles
-    @name = topic.topic
+  def search
+    keyword = params[:keyword]
+    if keyword == ""
+      redirect_to('/dashboard')
+      flash.notice = "Field cannot be blank"
+    else
+      topic = TopicFacade.get_articles(keyword)
+      article_array = topic.articles
+      @articles = article_array.shuffle
+      @name = topic.topic
+    end
   end
 end
