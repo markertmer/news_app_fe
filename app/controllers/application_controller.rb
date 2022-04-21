2f794460f88fb8c84b9da700fbf73f23f48fa721
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-21 16:55:26 -0500

    log_in and log_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :require_user, :logged_in?
 
   def current_user
     User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    current_user
   end
 
   def require_user
