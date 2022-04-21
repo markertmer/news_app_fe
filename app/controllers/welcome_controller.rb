64229ece42c19cb6383a38e57423034b097f1780
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-21 16:22:34 -0400

    Merge branch 'main' of github.com:markertmer/news_app_fe into blank_search

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,12 @@ class WelcomeController < ApplicationController
   def index
     keyword = ["biden", "putin", "supreme court", "will smith", "billionaires"].sample
     topic = TopicFacade.get_articles(keyword)
-    @articles = topic.articles
+    article_array = topic.articles
+    @articles = article_array.shuffle
     @name = topic.topic
+  end
+
+  def show
+
   end
 end
--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -42,39 +42,16 @@ RSpec.describe 'welcome index page' do
   end
 
   it 'displays three articles' do
-    # Need stub data to use for the content
-    # data =
     visit '/'
 
     expect(page).to have_content("Recent Stories on #{@data[:topic]}")
 
-    within('#left') do
-      expect(page).to have_link("Biden is Cool!") #headline
-      expect(page).to have_content("Top Ten reasons why we LOVE Joe Biden. Click to read more!") #summary
-      # expect(page).to have_content() #image
-      # find("img[alt='left-image']").click
-      # expect(current_path).to eq("http://msnbc.com/biden-rules") #left-article URL
-    end
-
-    visit '/'
-
-    within('#center') do
-      expect(page).to have_link("Joe Biden: Human?") #headline
-      expect(page).to have_content("We take a closer look at the personhood of the 46th president") #summary
-      # expect(page).to have_content() #image
-      find("img[alt='center-image']").click
-      # expect(current_path).to eq("http://ap.com/is-biden-human") #center-article URL
-    end
-
-    visit '/'
-
-    within('#right') do
-      expect(page).to have_link("Biden is Coming for your Children") #headline
-      expect(page).to have_content("Lock your doors! Load your guns! Hide your babies!") #summary
-      # expect(page).to have_content() #image
-      find("img[alt='right-image']").click
-      # expect(current_path).to eq("http://faux-newz.com/biden-sux") #right-article URL
-    end
+    expect(page).to have_link("Biden is Cool!") #headline
+    expect(page).to have_content("Top Ten reasons why we LOVE Joe Biden. Click to read more!") #summary
+    expect(page).to have_link("Joe Biden: Human?") #headline
+    expect(page).to have_content("We take a closer look at the personhood of the 46th president") #summary
+    expect(page).to have_link("Biden is Coming for your Children") #headline
+    expect(page).to have_content("Lock your doors! Load your guns! Hide your babies!") #summary
   end
 
   it 'sign-up/sign-in button present when user is not logged in' do
