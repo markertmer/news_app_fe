5a2a1796a04a330c16e6e9875406ad084a9a5a05
Ruben Pasillas <hobbiathan@gmail.com>
2022-04-21 20:45:37 -0600

    create ::valid_characters_v2(str) for review

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -18,6 +18,17 @@ before_action :require_user
   end
 
   private
+    def valid_characters_v2(str)
+      # You know what, god bless Dennis Ritchie, but I hate Regex
+      !(str =~ /[ \$ \: \; \* \! \# \@ \% \^ \& \( \) \? \> \< \. \[ \] \\ \/ ]/)
+    end
+
+    # User gives us a keyword
+    # keyword -> backend
+    # keyword.contains($:;*, etc...) -> backend breaks
+    # find_valid_characters(keyword)
+    # find_valid_characters(keyword) = true IF keyword.does_not_contain ["$", ":", ";", "*", "!", "#", "@", "%", "^", "&", "(", ")", "?", ">", "<", ".", "'", "[", "]"]
+
     def valid_characters(keyword)
       array = ["$", ":", ";", "*", "!", "#", "@", "%", "^", "&", "(", ")", "?", ">", "<", ".", "'", "[", "]"]
       valid = true
